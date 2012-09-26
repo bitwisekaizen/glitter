171021c75575b2bb4dc7813617451c8ecfa51536
Gavin Gray <gavin@vmware.com>
2012-09-25 20:49:54 -0400

    First test to post and retrieve message.

--- backend/tests.py
+++ backend/tests.py
@@ -9,8 +9,11 @@ from django.test.client import Client
 from django.test import TestCase
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def canPostAndRetrieveMessage(self):
+        c = Client()
+
+        response = c.post("/messages", {'message' : 'This is my first Gleet!'})
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get("/messages")
+        self.assertEqual(response._body['message'], 'This is my first Gleet!')
